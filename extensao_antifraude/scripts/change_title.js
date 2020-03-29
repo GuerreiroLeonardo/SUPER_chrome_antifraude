f5a6c5b389c1315d6719a4639b02af9cd9b992f9
Lucas <lucasvtiradentes@gmail.com>
2020-03-29 08:55:03 -0300

    Coloquei títulos novos na aba

--- extensao_antifraude/scripts/change_title.js
+++ extensao_antifraude/scripts/change_title.js
@@ -4,6 +4,45 @@ var testa = document.getElementsByClassName(classe)[0]
 if(typeof testa === "undefined") {
   console.log("Página do super que não tem Pedidos")
 } else {
+  
+  var str_selecionada = document.getElementsByClassName("selected")[0].innerText
+  console.log(str_selecionada)
+
+  if (str_selecionada == "Pagamento em análise"){
+    str_selecionada_fix = "Análises"
+  } else if (str_selecionada == "Pagamento autorizado") {
+    str_selecionada_fix = "Autorizados"
+  } else if (str_selecionada == "Pedido aceito") {
+    str_selecionada_fix = "Aceitos"
+  } else if (str_selecionada == "Em separação") {
+    str_selecionada_fix = "Em separação"
+  } else if (str_selecionada == "Pedido separado") {
+    str_selecionada_fix = "Separados"
+  } else if (str_selecionada == "Pedido recebido pelo entregador") {
+    str_selecionada_fix = "Recebidos entregador"
+  } else if (str_selecionada == "Pronto para retirada") {
+    str_selecionada_fix = "Pronto p/ retirada"
+  } else if (str_selecionada == "Pronto para entrega") {
+    str_selecionada_fix = "Pronto p/ ntrega"
+  } else if (str_selecionada == "Saiu para entrega") {
+    str_selecionada_fix = str_selecionada
+  } else if (str_selecionada == "Entregue") {
+    str_selecionada_fix = "Entregues"
+  } else if (str_selecionada == "Não Entregue") {
+    str_selecionada_fix = "Não entregues"
+  } else if (str_selecionada == "Cancelado pelo Cliente") {
+    str_selecionada_fix = "Cancelados cli"
+  } else if (str_selecionada == "Cancelado") {
+    str_selecionada_fix = "Cancelados"
+  } else if (str_selecionada == "Pedido de entrega aceito pelo entregador") {
+    str_selecionada_fix = "Aceitos para entrega"
+  } else if (str_selecionada == "Pagamento finalizado") {
+    str_selecionada_fix = "Pagamento finalizado"
+  } else {
+    str_selecionada_fix = str_selecionada
+  }
+  
+  console.log(str_selecionada_fix)
 
   var str_pedidos = document.getElementsByClassName(classe)[0].innerHTML
   var num_pedidos = str_pedidos.trim()
@@ -11,9 +50,9 @@ if(typeof testa === "undefined") {
   num_pedidos = num_pedidos.replace(" Pedido", "")
   
     if(num_pedidos == "0"){
-      document.title = "_"
+      document.title = "_" + " " + str_selecionada_fix
     } else {
-      document.title = num_pedidos + " pedido(s)"
+      document.title = num_pedidos + " " + str_selecionada_fix
     }
   
 }
